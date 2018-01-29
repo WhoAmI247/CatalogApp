c52f01f73d2dbb46e20f3fdb43ed548a16afc0fb
WhoAmI247 <phudoancq@gmail.com>
2018-01-29 09:11:20 +0000

    Update link home in logo

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -70,6 +70,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:item_id, :name, :description)
+      params.require(:item).permit(:item_id, :name, :description, :category_id)
     end
 end
