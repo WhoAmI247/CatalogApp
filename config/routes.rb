34aa15323b95f0ea007612713c195091d8b64d9e
WhoAmI247 <phudoancq@gmail.com>
2018-01-29 08:32:22 +0000

    Finish create UI Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'categories#index'
+  
+  get 'signup', to: 'users#new'
+  resources :users
 end
